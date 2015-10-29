980e5ba314c12883dac8b5a5bc60193b10e781e1
LeifBernsdorf <leif.bernsdorf@tariwe.de>
2015-10-29 10:35:24 +0100

    Added View change on logout

--- app/src/main/java/com/leruka/leruka/UserMainActivity.java
+++ app/src/main/java/com/leruka/leruka/UserMainActivity.java
@@ -1,5 +1,6 @@
 package com.leruka.leruka;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,6 +25,11 @@ public class UserMainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void logOut(View view) {
+        Intent intent = new Intent(this, GuestMainActivity.class);
+        startActivity(intent);
     }
 
 }
