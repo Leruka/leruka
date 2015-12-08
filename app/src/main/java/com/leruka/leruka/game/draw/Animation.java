632b371e5f447787441dd0db0c408914a0b07533
RuWe <Ruthweber95@web.de>
2015-12-08 13:31:01 +0100

    CHANGE: In Animation Klasse Funktion update() geändert

--- app/src/main/java/com/leruka/leruka/game/draw/Animation.java
+++ app/src/main/java/com/leruka/leruka/game/draw/Animation.java
@@ -11,22 +11,34 @@ public class Animation {
     private int totalFrames;
     private int currentFrame;
     private Bitmap[] frames;
-    private int repeats;
+    private int currentTime;
+    private int[] repeats;
 
     // Constructor
-    public Animation(Bitmap[] frames, int repeats) {
+    public Animation(Bitmap[] frames, int[] repeats) {
         this.frames = frames;
         this.totalFrames = frames.length;
         this.currentFrame = 0;
         this.repeats = repeats;
+
     }
 
     public Bitmap currentFrame() {
+        currentTime = repeats[currentFrame];
         return this.frames[this.currentFrame];
     }
 
     public void update() {
-        //TODO
+        if (currentTime != 0) {
+            currentTime = currentTime - 1;
+        } else {
+            currentFrame = currentFrame + 1;
+            currentFrame();
+        }
+        if (currentFrame >= totalFrames) {
+            currentFrame = 0;
+            currentFrame();
+        }
     }
 
     public void reset() {
